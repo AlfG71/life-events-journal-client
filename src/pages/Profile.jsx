3b55d5745f0d1db7a9a89b52921780f6c0b22816
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-10-23 11:02:50 -0700

    refactored Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import EditProfile from "../components/EditProfile";
+import DeleteProfile from "../components/DeleteProfile";
 
 
 const Profile = () => {
@@ -11,48 +12,38 @@ const Profile = () => {
 
   const [isEditing, setIsEditing] = useState(false)
 
+  const [willDelete, setWillDelete] = useState(false)
+
   const handleEditClick = () => {
     setIsEditing(true);
   }
 
-  const experimenting = () => {
-    console.log("Clicking Link Tag")
+  const handleWillDelete = () => {
+    setWillDelete(true);
   }
 
-  return (
-    <div>
-
-      { isEditing ? (
-        <EditProfile user={user} setIsEditing={setIsEditing} />
-        ) : (
-        <>
-          <h1>Profile</h1>
-
-          {user &&
-
-
+  if (isEditing) {
+    return <EditProfile user={user} setIsEditing={setIsEditing} />;
+  } else if (willDelete) {
+    return <DeleteProfile user={user} setWillDelete={setWillDelete} />;
+  } else {
+    return (
+      <>
+        <h1>Profile</h1>
+        {user && (
           <>
-
             <h2>Welcome {user.userName}!</h2>
             <img className="user-image" src={user.img} />
             <p>{user.children}</p>
-
           </>
-
-
-          }
-
-
-          <Link to='/add-child'>Add Child</Link>
-<Link onClick={handleEditClick} >Experiment</Link>
-          <button onClick={handleEditClick}>Edit Profile</button>
-          {/* <Link to='/update' onClick={handleEditClick}>Edit Profile</Link> */}
-          <Link to='/delete'>Delete Profile</Link>
-          <Link to='/login'>Logout</Link>  {/* explore more, not sure how to work it */}
-        </>
-      )}
-    </div>
-  )
+        )}
+        <Link to="/add-child">Add Child</Link>
+        <Link onClick={handleEditClick}>Edit Profile</Link>
+        <Link onClick={handleWillDelete}>Delete Profile</Link>
+        <Link to="/signup">Logout</Link>
+      </>
+    );
+  }
 }
 
 export default Profile
