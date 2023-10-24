198593b9df65325e65ae0bcedbfccaa7112e92af
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-10-24 10:10:28 -0700

    delete child

---
+++ src/components/ChildProfileDelete.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { axiosDelete } from '../services/authService';
+
+function ChildProfileDelete({ childId }) {
+
+  const navigate = useNavigate()
+
+
+  const deleteChildProfile = async () => {
+    const confirmDelete = window.confirm('Do you really want to delete this child profile?');
+    if (confirmDelete) {
+      axiosDelete(`/children/${childId}`)
+      .then(data => {
+        alert('Child information deleted.');
+        navigate('/profile');
+      })
+      .catch((error) => {
+        alert(error.message || 'An error occurred while deleting the child profile.');
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h3>Child Profile</h3>
+      <button onClick={deleteChildProfile}>Delete Profile</button>
+    </div>
+  );
+}
+
+export default ChildProfileDelete;
--- src/pages/ChildProfile.jsx
+++ src/pages/ChildProfile.jsx
@@ -1,21 +1,40 @@
 import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
 import differenceInYears from 'date-fns/differenceInYears';
 import EditChild from '../components/EditChild';
+import { axiosDelete } from '../services/authService'; // added
 
 const ChildProfile = () => {
     const { childId } = useParams();
-    const {user} = useContext(AuthContext)
+    const {user, setUser, storeToken} = useContext(AuthContext) // added setUser, storeToken
     const [isEditing, setIsEditing] = useState(false)
+
+    const navigate = useNavigate();
 
     const handleEditClick = () => {
       setIsEditing(true);
     }
 
-    console.log(user)
+    console.log("User ===>", user)
     const child = user && user.children.find((item) => item._id === childId)
     const date = child && new Date(child.dateOfBirth.replace(/-/g, '\/').replace(/T.+/, ''))
+
+    const deleteChildProfile = async () => {  // Added method
+      const confirmDelete = window.confirm('Do you really want to delete this child profile?');
+      if (confirmDelete) {
+        axiosDelete(`/child/delete/${childId}`)
+        .then(response => {
+          setUser(response.data.user)
+          storeToken(response.data.authToken)
+          alert('Child information deleted.');
+          navigate('/profile');
+        })
+        .catch((error) => {
+          alert(error.message || 'An error occurred while deleting the child profile.');
+        });
+      }
+    };
 
     if (isEditing) {
       return <EditChild user={child} setIsEditing={setIsEditing} />;
@@ -27,6 +46,7 @@ const ChildProfile = () => {
           <p>Age: {Math.abs(differenceInYears(date, Date.now()))} </p>
           <img src={child.img} alt="Child's profile" />
           <button onClick={handleEditClick}>Edit Profile</button>
+          <button onClick={deleteChildProfile}>Delete Profile</button>
         </div>
       ) : (
         <p>loading..</p>
