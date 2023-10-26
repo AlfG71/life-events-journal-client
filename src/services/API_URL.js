3790bc801ddac0b0169d7ad3dac20b2b9fdf728f
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-10-26 15:27:39 -0700

    Deploy Fixed add child picture bug

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,3 +1,3 @@
-// export const API_URL = "http://localhost:3000"  // local dev
+// export const API_URL = "http://localhost:3000"  // local dev URL for testing (switch to dploy URL before committing to GitHub)
 
-export const API_URL = "https://lifeeventsjournal.adaptable.app"
+export const API_URL = "https://lifeeventsjournal.adaptable.app" // deploy URL (Switch to local URL for testing during development)
